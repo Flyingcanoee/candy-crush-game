d0204807e12a6204a5adf0dbde361388679e0fd5
Flyingcanoee <nastya_tkachenko@list.ru>
2022-05-26 15:29:04 +0300

    Build for github pages

---
+++ docs/index.816e7b21.js
@@ -0,0 +1,4 @@
+const Application = PIXI.Application;
+const App = new Application();
+
+//# sourceMappingURL=index.816e7b21.js.map
---
+++ docs/index.bcf80803.js
@@ -0,0 +1,2 @@
+console.log("I m here");
+//# sourceMappingURL=index.bcf80803.js.map
